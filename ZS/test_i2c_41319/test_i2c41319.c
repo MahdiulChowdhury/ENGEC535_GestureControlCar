25d3ef85d0b0a077c42f1b56729791afa8005f23
Safran <[zsafran@bu.edu]>
2019-04-15 13:50:05 -0400

    updated i2c test ul

--- ZS/test_i2c_41319/test_i2c41319.c
+++ ZS/test_i2c_41319/test_i2c41319.c
@@ -28,23 +28,32 @@
 #define ADDRESS 0x40
 
 /* function prototypes */
+void pwmBegin(int file);
 void delay(int milliseconds);
-void reset();
+void pwmReset();
 void setPwmFreq(int file, float freq);
-void setPwm(uint8_t num, uint16_t on, uint16_t off);
+void setPwm(int file, uint8_t num, uint16_t on, uint16_t off);
 uint8_t read8(int file, uint8_t reg_addr);
 void write8(int file, uint8_t reg_addr, uint8_t data);
 
-int fd;
 
 int main(){
+	int fd;
 	float test = 9.0/2;
+	int pwmnum = 1;
 	printf("test = %.2f \n", test);
 	//delay(10000);
 	printf("test = %.2f \n + 1 ", test + 1);
+	pwmBegin(fd);
+	setPwm(fd, pwmnum, 0, 530);
 }
 
-void reset(int file){
+void pwmBegin(int file){
+	pwmReset();
+	setPwmFreq(file, 54);
+}
+
+void pwmReset(int file){
 	write8(file, PCA9685_MODE1, 0x80);
 	delay(10);
 }
@@ -52,12 +61,13 @@ void reset(int file){
 void write8(int file, uint8_t reg_addr, uint8_t data){
 	file = open("/dev/i2c-0", O_RDWR);
 
-	if( ioctl(fd, I2C_SLAVE, ADDRESS ) < 0)
+	if( ioctl(file, I2C_SLAVE, ADDRESS ) < 0)
 	{
 		fprintf( stderr, "Failed to set slave address: %m\n" );
 	}
 	
 	i2c_smbus_write_byte_data(file, reg_addr, data);
+	close(file);
 	
 }
 
@@ -65,7 +75,7 @@ uint8_t read8(int file, uint8_t reg_addr) {
 
 	file = open("/dev/i2c-0", O_RDWR);
 
-	if( ioctl(fd, I2C_SLAVE, ADDRESS ) < 0)
+	if( ioctl(file, I2C_SLAVE, ADDRESS ) < 0)
 	{
 		fprintf( stderr, "Failed to set slave address: %m\n" );
 	}
@@ -84,6 +94,7 @@ void setPwmFreq(int file, float freq){
 
 	uint8_t prescale = floor(prescaleval + 0.5);
 	uint8_t oldmode = read8(file, PCA9685_MODE1);
+	close(file);
 	uint8_t newmode = (oldmode & 0x7F) | 0x10; // sleep
 	write8(file, PCA9685_MODE1, newmode);            // go to sleep
 	write8(file, PCA9685_PRESCALE, prescale);        // set the prescaler
@@ -91,6 +102,18 @@ void setPwmFreq(int file, float freq){
 	delay(5);
 	write8(file, PCA9685_MODE1, oldmode |0xa0); //  This sets the MODE1 register to turn on auto increment.
 
+}
+
+void setPwm(int file, uint8_t num, uint16_t on, uint16_t off){
+	/*write8(file, LED0_ON_L + 4 * num);
+	write8(file, on);
+	write8(file, on >> 8);
+	write8(file, off);
+	write8(file, off >> 8);	*/
+	write8(file, LED0_ON_L + 4 * num, on);
+	write8(file, LED0_ON_L + 4 * num + 1, on >> 8);
+	write8(file, LED0_ON_L + 4 * num + 2, off);
+	write8(file, LED0_ON_L + 4 * num + 3, off >> 8);
 }
 
 void delay(int milliseconds)
