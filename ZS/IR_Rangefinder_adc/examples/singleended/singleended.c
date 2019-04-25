68d1a503727ff4d87583ca42004af631ff4129fe
Zachary Safran <zsafran@bu.edu>
2019-04-24 20:23:48 -0400

    initial adc lib convert code

--- ZS/IR_Rangefinder_adc/examples/singleended/singleended.c
+++ ZS/IR_Rangefinder_adc/examples/singleended/singleended.c
@@ -1,17 +1,17 @@
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
- Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-//Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+// Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
 
-void setup(void)
+void setup(void) 
 {
   Serial.begin(9600);
   Serial.println("Hello!");
-
+  
   Serial.println("Getting single-ended readings from AIN0..3");
   Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-
+  
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
@@ -24,11 +24,11 @@ void setup(void)
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
+  
   ads.begin();
 }
 
-void loop(void)
+void loop(void) 
 {
   int16_t adc0, adc1, adc2, adc3;
 
@@ -41,6 +41,6 @@ void loop(void)
   Serial.print("AIN2: "); Serial.println(adc2);
   Serial.print("AIN3: "); Serial.println(adc3);
   Serial.println(" ");
-
+  
   delay(1000);
 }
